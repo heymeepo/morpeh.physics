bb93343a040ee149ac66d3493e22ec752bcfbb1c
heymeepo <h3ystudio@gmail.com>
2024-01-23 13:22:59 +0400

    Add PhysicsFeature

---
+++ Runtime/ECS/Feature/PhysicsFeature.cs
@@ -0,0 +1,16 @@
+﻿#if MORPEH_ELYSIUM
+using Scellecs.Morpeh.Elysium;
+
+namespace Scellecs.Morpeh.Physics
+{
+    public sealed class PhysicsFeature : IEcsFeature
+    {
+        public void Configure(EcsStartup.FeatureBuilder builder)
+        {
+            builder
+                .AddInitializer(new PhysicsStateInitializer())
+                .AddFixedSystem(new PhysicsSimulationSystem());
+        }
+    }
+}
+#endif
